57d8ec2989a87e52197ce147234a3dd37e6ca2b8
George Burkhardt <burkhardtr@msoe.edu>
2020-06-08 21:39:45 -0500

    Redid import and added ignore

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	arrowParens: 'avoid'
+};
--- src/helpers.js
+++
@@ -1,7 +0,0 @@
-export function getDaysFromYearAndMonth(year, month, days) {
-	let numberOfDays = days[month];
-	if (Number(year) % 4 === 0 && month === 'February' && year != '1900') {
-		numberOfDays = days['February_LEAP'];
-	}
-	return numberOfDays;
-}
